6513de3fd3787dc0bb8be915815ff2ce6bf67321
Ramesh Sen <rjsen@me.com>
2013-11-06 01:23:24 -0800

    more aquamacs stuff

---
+++ customizations.el
@@ -0,0 +1,47 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 215 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(blink-cursor-mode nil)
+ '(cursor-type box)
+ '(default-frame-alist (quote ((tool-bar-lines . 0) (menu-bar-lines . 1) (background-color . "black") (left . 0) (width . 140) (height . 70) (cursor-type . box))))
+ '(font-lock-global-modes t)
+ '(font-lock-verbose t)
+ '(global-font-lock-mode t nil (font-lock))
+ '(js-indent-level 2)
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(nxhtml-skip-welcome t)
+ '(one-buffer-one-frame-mode t nil (aquamacs-frame-setup))
+ '(tool-bar-mode nil)
+ '(transient-mark-mode t)
+ '(visual-line-mode nil t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :background "black" :foreground "grey" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal))))
+ '(cperl-array-face ((((class color) (background dark)) (:foreground "rgb:90/d0/60"))))
+ '(cperl-hash-face ((((class color) (background dark)) (:foreground "rgb:d4/aa/59"))))
+ '(cperl-nonoverrideable-face ((((class color) (background dark)) (:foreground "rgb:00/00/ff"))))
+ '(cursor ((t (:background "grey" :foreground "black"))))
+ '(cursor-type box)
+ '(custom-face-tag ((t (:underline t))))
+ '(font-lock-builtin-face ((((class color) (background dark)) (:foreground "rgb:dd/00/ff"))))
+ '(font-lock-comment-face ((((class color) (background dark)) (:foreground "rgb:00/bb/00"))))
+ '(font-lock-constant-face ((((class color) (background dark)) (:foreground "Aquamarine"))))
+ '(font-lock-doc-face ((t (:foreground "rgb:22/77/22"))))
+ '(font-lock-function-name-face ((((class color) (background dark)) (:foreground "rgb:33/99/dd"))))
+ '(font-lock-keyword-face ((((class color) (background dark)) (:foreground "rgb:99/55/ff"))))
+ '(font-lock-preprocessor-face ((((class color) (background dark)) (:foreground "rgb:33/cc/ff"))))
+ '(font-lock-reference-face ((((class color) (background dark)) (:foreground "rgb:55/aa/aa"))))
+ '(font-lock-string-face ((((class color) (background dark)) (:foreground "salmon"))))
+ '(font-lock-type-face ((((class color) (background dark)) (:foreground "rgb:cc/44/66"))))
+ '(font-lock-variable-name-face ((((class color) (background dark)) (:foreground "rgb:d0/dd/70"))))
+ '(font-lock-warning-face ((((class color) (background dark)) (:bold t :foreground "red"))))
+ '(paren-face-match-light ((((class color)) nil))))
